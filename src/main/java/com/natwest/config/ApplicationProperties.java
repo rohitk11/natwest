5818b00ec74b5474d36e7297c7b1155fa7e9fdfc
rajxd <raj.garande@gmail.com>
2022-04-04 00:29:30 +0530

    Initial version of natwest generated by generator-jhipster@7.7.0

---
+++ src/main/java/com/natwest/config/ApplicationProperties.java
@@ -0,0 +1,12 @@
+package com.natwest.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Natwest.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {}
---
+++ src/main/java/com/natwest/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package com.natwest.security;
+
+import com.natwest.config.Constants;
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM));
+    }
+}
